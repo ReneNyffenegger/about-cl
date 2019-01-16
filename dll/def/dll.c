3102f2d4ac0850918a4b290d41100df38e8757c9
René Nyffenegger <mail@renenyffenegger.ch>
2019-01-16 15:07:08 +0100

    dll with def file

---
+++ dll/def/dll.c
@@ -0,0 +1,5 @@
+#include "dll.h"
+
+int __stdcall add(int a, int b) {
+  return a+b;
+}
---
+++ dll/def/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "dll.h"
+
+int main() {
+
+  printf("18+24 = %d\n", add(18, 24));
+
+}
